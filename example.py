d3e87fa740b2b9f79542bf7c9d6d46a2a0535e25
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-04-03 15:47:51 +0900

    add example

---
+++ example.py
@@ -0,0 +1,9 @@
+from pyops.init import *
+
+if "patch" == sys.argv[1] :
+  versioning  = sys.argv[2:]
+  check_commit(versioning)
+
+  run_command("poetry build --format wheel", "building dist")
+  latest = get_latest_file("/dist/*.whl")
+  run_command(f"pip install --upgrade /dist/{latest}", "install upgrade")
