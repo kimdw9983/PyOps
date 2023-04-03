cfe19bdd8fc7ad143aac7c9e7f6b7dd7a42f4278
kimdw9983 <93891414+kimdw9983@users.noreply.github.com>
2023-04-03 15:47:47 +0900

    get_latest_file

--- pyops/init.py
+++ pyops/init.py
@@ -1,4 +1,4 @@
-import subprocess, sys, re, signal, os
+import subprocess, sys, re, signal, os, glob
 
 IS_WINDOWS = os.name == 'nt'
 
@@ -58,7 +58,10 @@ def find_regex(pattern: str, text: str) -> str | None :
     return print("regex matches more than one, try change pattern.")
   return match[0] if match else None
 
-def check_commit() -> None :
+def check_commit(versioning: str) -> None :
+  if versioning in ("no_change", "no_version") :
+    return print(f"{blue}INFO{reset}\tVersioning is disabled, ignoring.")
+
   output, _ = run_command("git status", "git commit check")
   commit = find_regex(r"Changes not staged for commit:", output)
   if commit :
@@ -68,3 +71,12 @@ def check_commit() -> None :
     title = input(f"{yellow}INPUT\t{reset}")
     run_command('git add .', f'{blue}git add .')
     run_command(f'git commit -am "{title}"', f'{blue}git commit -am "{title}"')
+
+def get_latest_file(pattern: str="", directory: str="") -> str | None :
+  if not pattern and not directory :
+    print(f"{red}[ERROR]\t{yellow}get_latest_file() {red}requires pattern or directory")
+    sys.exit(0)
+
+  files = glob.glob(directory or os.getcwd() + pattern)
+  latest = max(files, key=os.path.getctime).split("\\")[-1].replace("\\", "/")
+  return latest
